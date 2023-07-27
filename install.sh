cbdc221559275467c18c663d92c05dbeeb8b8aed
ginschel <140172310+ginschel@users.noreply.github.com>
2023-07-27 14:14:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@
 mv classist_files/ ~/.local/bin
 mv classist ~/.local/bin
 chmod +x ~/.local/bin/classist
-echo "classist has been installed"
+cd ~/.local/bin/classist_files
+python -m venv classist
+source ~/.local/bin/classist_files/classist/bin/activate
+pip install python-dotenv
+pip install langchain
+pip install huggingface_hub
+echo "classist has been installed. Please set your API token with -c"
